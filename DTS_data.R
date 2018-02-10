5495f1b603834c5b4f714537d1bbb400b341969d
bondnoaa <lea.bond>
2018-02-10 11:02:19 -0800

    moved DTS file and some updates to axes labels, etc.

--- DTS_data.R
+++ DTS_data.R
@@ -34,8 +34,8 @@ str(out)
 
 #3. Get DTS data into timestamp form rather then multiple columns
 dts_dt <- out  %>% 
-  mutate(timestamp = make_datetime(year, month, day, hour, minute)) %>% 
-  select(long, long2, timestamp, temp)
+  mutate(tmestamp = make_datetime(year, month, day, hour, minute)) %>% 
+  select(long, long2, tmestamp, temp)
 
 dts_hour <- out %>% 
   mutate(date = make_date(year, month, day)) %>% 
@@ -43,14 +43,18 @@ dts_hour <- out %>%
   group_by(long, date, hour) %>% 
   summarise(meantemp = mean(temp))
 
-str(dts_hour)
+str(dts_dt)
 
 ####Now make a heatmap in ggplot ####
 # 1m pixels (all data)
-ggplot(dts_dt, aes(long,timestamp, fill = temp)) + 
+#dts_1m <- 
+  ggplot(dts_dt, aes(long,tmestamp, fill = temp)) + 
   geom_tile() +
   theme_bw() +
-  scale_fill_viridis()
+  scale_fill_viridis() +
+  labs(x = "Distance downstream [m]", y = "Date") +
+  scale_x_continuous(limits = c(1,850)) 
+  #scale_y_date(date_labels = "%d")
 
 # Hourly pixles 
 ggplot(dts_hour, aes(long,date, fill = meantemp)) + 
@@ -62,3 +66,63 @@ ggplot(dts_hour, aes(long,date, fill = meantemp)) +
 #+ scale_colour_gradient(low = "#01665e", high = "#8c510a",guide = "colourbar")
 #scale_color_continuous("#01665e","#f5f5f5","#8c510a")
 
+#### Hourly mean temperature line plot ####
+meantemp <- dts_dt %>% 
+  select(long, long2, timestamp, temp) %>% 
+  group_by(timestamp) %>% 
+  mutate( mtemp = mean(temp)) %>% 
+  ungroup(timestamp)  
+
+
+#temp_mean <- 
+  ggplot(meantemp, aes(timestamp, mtemp)) +
+  geom_line() + 
+  theme_bw()  +
+  labs( x = "Date", y = "Mean Temperature [*C]")
+
+#Plot with raw data - NOTE: it will take a while to load.
+ggplot(meantemp, aes(timestamp, mtemp)) +
+  geom_line() + 
+  #geom_point(aes(timestamp, temp)) +
+  theme_bw()  +
+  labs(x = "Date", y = "Mean Temperature [*C]")
+  
+
+# #Stacking multiple plots together
+# multiplot <- function(..., plotlist=NULL, file, cols=1, layout=NULL) {
+#   library(grid)
+#   
+#   # Make a list from the ... arguments and plotlist
+#   plots <- c(list(...), plotlist)
+#   
+#   numPlots = length(plots)
+#   
+#   # If layout is NULL, then use 'cols' to determine layout
+#   if (is.null(layout)) {
+#     # Make the panel
+#     # ncol: Number of columns of plots
+#     # nrow: Number of rows needed, calculated from # of cols
+#     layout <- matrix(seq(1, cols * ceiling(numPlots/cols)),
+#                      ncol = cols, nrow = ceiling(numPlots/cols))
+#   }
+#   
+#   if (numPlots==1) {
+#     print(plots[[1]])
+#     
+#   } else {
+#     # Set up the page
+#     grid.newpage()
+#     pushViewport(viewport(layout = grid.layout(nrow(layout), ncol(layout))))
+#     
+#     # Make each plot, in the correct location
+#     for (i in 1:numPlots) {
+#       # Get the i,j matrix positions of the regions that contain this subplot
+#       matchidx <- as.data.frame(which(layout == i, arr.ind = TRUE))
+#       
+#       print(plots[[i]], vp = viewport(layout.pos.row = matchidx$row,
+#                                       layout.pos.col = matchidx$col))
+#     }
+#   }
+# }
+# 
+# multiplot(dts_1m,temp_mean,1)
